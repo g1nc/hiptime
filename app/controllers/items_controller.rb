dae12e233e2998ca7f5bc4078821581ec5cb75a7
g1nc <grimm.hellg@gmail.com>
2016-09-24 20:20:49 +0300

    User_id to Item and Associations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,18 +2,18 @@ class ItemsController < ApplicationController
 	before_action :find_item, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@items = Item.all.order('created_at DESC')
+		@items = current_user.items.order('created_at DESC')
 	end
 
 	def show
 	end
 
 	def new
-		@item = Item.new
+		@item = current_user.items.build
 	end
 
 	def create
-		@item = Item.new(item_params)
+		@item = current_user.items.build(item_params)
 		if @item.save
 			redirect_to root_path
 		else
